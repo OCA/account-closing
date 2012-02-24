5f01523642f16ab95ac65e4d948fa111ce187ea0
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2012-02-24 17:47:09 +0100

    [IMP] account_unrealized_currency_gain_loss - [IMP]Reevaluation over any account selected by a boolean in account instead of only account with secondary currency / [ADD] currency_rate_type in res_currency_rate form / [ADD] a settable default currency reevaluation journal in company settings (lp:c2c-addons/6.1  rev 29)

--- account_unrealized_currency_gain_loss/__openerp__.py
+++ account_unrealized_currency_gain_loss/__openerp__.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Yannick Vaucher
-#    Copyright 2012 Camptocamp SA
+# Copyright (c) 2012 Camptocamp SA (http://www.camptocamp.com)
+# All Right Reserved
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# Author : Yannick Vaucher (Camptocamp)
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -27,39 +36,36 @@ Add a wizard to generate the unrealized currency gain & loss entries. This adjus
 
 To configure it, Foreign currency gain & loss account have been added in company parameters.
 
-For UK (Revaluation) :
+For UK (Reevaluation) :
                         LOSS  GAIN
-- Revaluation account   [x]  [x]
+- Reevaluation account   [x]  [x]
 - Provision B.S account  [ ]  [ ]
 - Provision P&L account  [ ]  [ ]
 
 For CH (Provision) :
                         LOSS  GAIN
-- Revaluation account   [ ]  [ ]
+- Reevaluation account   [ ]  [ ]
 - Provision B.S account  [x]  [ ]
 - Provision P&L account  [x]  [ ]
 
 For FR
                         LOSS  GAIN
-- Revaluation account   [x]  [x]
+- Reevaluation account   [x]  [x]
 - Provision B.S account  [x]  [ ]
 - Provision P&L account  [x]  [ ]
 
 """,
 
     "author": "Camptocamp",
-    "license": 'AGPL-3',
     "depends": ["base",
                 "account",
-                "account_reversal",
-                "base_headers_webkit"],
+                "account_reversal"],
     "init_xml": ["res_company_view.xml",
                  "res_currency_view.xml",
                  "account_view.xml",
-                 "wizard/wizard_currency_revaluation_view.xml",
-                 "wizard/print_currency_unrealized_report_view.xml"],
-    "update_xml": ['report/report.xml'],
-    #"test": ["test/currency_revaluation.yml"],
+                 "wizard_currency_reevaluation_view.xml"],
+    "update_xml": [],
+    "test": ["test/currency_reevaluation.yml"],
     "demo_xml": [],
     "installable": True,
     "active": False,
