1fa9631ed2bd3a8ac9d27276913b739c8c710f32
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-01-14 18:04:13 +0100

    [IMP] remove test print

--- account_unrealized_currency_gain_loss/wizard/print_currency_unrealized_report.py
+++ account_unrealized_currency_gain_loss/wizard/print_currency_unrealized_report.py
@@ -41,13 +41,13 @@ class UnrealizedCurrencyReportPrinter(orm.TransientModel):
         # we update form with display account value
         if isinstance(wid, list):
             wid = wid[0]
-        current = self.browse(cursor, uid, wid, context)
+        current = self.browse(cursor, uid, wid, context=context)
         form = {}
         form['period_id'] = current.period_id.id
         form['period_name'] = current.period_id.name
         form['account_ids'] = [current.chart_account_id.id]
         data['form'] = form
-        print data
+
         return {'type': 'ir.actions.report.xml',
                 'report_name': 'currency_unrealized',
                 'datas': data}
