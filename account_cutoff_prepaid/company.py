f94502f8a3f5d5d14ac8e77d5070bed6a370dd60
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-11-28 14:00:18 +0100

    [MIG] move account_cutoff_base and account_cutoff_prepaid out of unported

---
+++ account_cutoff_prepaid/company.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Account Cut-off Prepaid module for OpenERP
+#    Copyright (C) 2013 Akretion (http://www.akretion.com)
+#    @author Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from openerp.osv import orm, fields
+
+
+class res_company(orm.Model):
+    _inherit = 'res.company'
+
+    _columns = {
+        'default_prepaid_revenue_account_id': fields.many2one(
+            'account.account', 'Default Account for Prepaid Revenue',
+            domain=[('type', '<>', 'view'), ('type', '<>', 'closed')]),
+        'default_prepaid_expense_account_id': fields.many2one(
+            'account.account', 'Default Account for Prepaid Expense',
+            domain=[('type', '<>', 'view'), ('type', '<>', 'closed')]),
+    }
