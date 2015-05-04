f4dab40c44ef9e6153611c859a4ec202b3538e35
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-05-04 14:02:13 +0200

    [8.0] migrate account_cutoff_prepaid and account_accrual_base
    Migration by @alexis-via
    
    @sbidoul added:
    
    *    extensibility hooks
    *    a multi-company record rule
    *    some tests
    
    @gurneyalex moved the module desciption to README.rst as part of the merge

--- account_cutoff_prepaid/__openerp__.py
+++ account_cutoff_prepaid/__openerp__.py
@@ -27,23 +27,6 @@
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Prepaid Expense, Prepaid Revenue',
-    'description': """
-Manage prepaid expense and revenue based on start and end dates
-===============================================================
-
-This module adds a **Start Date** and **End Date** field on invoice lines. For
-example, if you have an insurance contrat for your company that run from April
-1st 2013 to March 31st 2014, you will enter these dates as start and end dates
-on the supplier invoice line. If your fiscal year ends on December 31st 2013,
-3 months of expenses are part of the 2014 fiscal year and should not be part of
-the 2013 fiscal year. So, thanks to this module, you will create a *Prepaid
-Expense* on December 31st 2013 and OpenERP will identify this expense with the
-3 months that are after the cut-off date and propose to generate the
-appropriate cut-off journal entry.
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com>
-for any help or question about this module.
-    """,
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com',
     'depends': ['account_cutoff_base'],
@@ -63,4 +46,5 @@ for any help or question about this module.
     'installable': True,
     'active': False,
     'application': True,
+    'license': 'AGPL-3'
 }
