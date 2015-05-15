97675f46124e622f994cd2296fecf1d5c68beddc
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-05-15 18:14:58 +0200

    False isn't necessary

--- account_invoice_accrual/account_invoice.py
+++ account_invoice_accrual/account_invoice.py
@@ -36,7 +36,7 @@ class account_invoice_line(orm.Model):
     def move_line_get_item(self, cr, uid, line, context=None):
         res = super(account_invoice_line, self)\
             .move_line_get_item(cr, uid, line, context=context)
-        if context.get('move_accrual', False) and context.get('type', False):
+        if context.get('move_accrual') and context.get('type'):
             mapping_obj = self.pool['account.cutoff.mapping']
             if context.get('type', False) in ['out_invoice', 'out_refund']:
                 mapping_type = 'accrued_revenue'
