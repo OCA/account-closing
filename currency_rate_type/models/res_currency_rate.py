5f2639c7cd71cb56df6b35ce73a190cc18cbf492
Levent Karakas <leventk@eskayazilim.com.tr>
2015-12-21 15:57:26 +0200

    revaluation rate type

---
+++ currency_rate_type/models/res_currency_rate.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015, Eska Yazılım ve Danışmanlık A.Ş.
+#    http://www.eskayazilim.com.tr
+#
+#    Copyright (C) 2014 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning
+
+
+class ResCurrencyRate(models.Model):
+    _inherit = "res.currency.rate"
+
+    currency_rate_type_id = fields.Many2one(
+        'res.currency.rate.type',
+        'Currency Rate Type',
+        help="Allow you to define your own currency rate types, like 'Average' or 'Year to Date'. "
+             "Leave empty if you simply want to use the normal 'spot' rate type")
---
+++ currency_rate_type/models/res_currency_rate_type.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015, Eska Yazılım ve Danışmanlık A.Ş.
+#    http://www.eskayazilim.com.tr
+#
+#    Copyright (C) 2014 Smile (<http://www.smile.fr>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning
+
+
+class ResCurrencyRateType(models.Model):
+    _name = "res.currency.rate.type"
+    _description = "Currency Rate Type"
+
+    name = fields.Char("Name", size=64, required=True, translate=True)
