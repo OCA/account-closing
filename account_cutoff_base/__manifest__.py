e14503b3b96a192ba6fbc2e69906735a58300e02
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-25 01:03:07 +0200

    Port account_cutoff_base and account_cutoff_prepaid to Odoo v9
    Extract a new module account_invoice_start_end_dates from account_cutoff_prepaid

--- account_cutoff_base/__manifest__.py
+++ account_cutoff_base/__manifest__.py
@@ -1,54 +1,21 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Account Cut-off Base module for OpenERP
-#    Copyright (C) 2013 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Cut-off Base',
-    'version': '8.0.0.1.0',
+    'version': '9.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Base module for Account Cut-offs',
-    'description': """
-This module contains objets, fields and menu entries that are used by other
-cut-off modules. So you need to install other cut-off modules to get the
-additionnal functionalities :
-
-* the module *account_cutoff_prepaid* will manage prepaid cut-offs based on
-  start date and end date,
-* the module *account_cutoff_accrual_picking* will manage the accruals based
-  on the status of the pickings.
-
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com>
-for any help or question about this module.
-    """,
-    'author': "Akretion,Odoo Community Association (OCA)",
+    'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': ['account_accountant'],
     'data': [
-        'company_view.xml',
-        'account_cutoff_view.xml',
-        'security/ir.model.access.csv',
         'security/account_cutoff_base_security.xml',
+        'security/ir.model.access.csv',
+        'views/company.xml',
+        'views/account_cutoff.xml',
     ],
-    'installable': False,
-    'active': False,
+    'installable': True,
 }
---
+++ account_cutoff_base/models/account_cutoff.py
@@ -0,0 +1,312 @@
+# -*- coding: utf-8 -*-
+# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, _
+from openerp.exceptions import UserError
+from collections import defaultdict
+
+
+class AccountCutoff(models.Model):
+    _name = 'account.cutoff'
+    _rec_name = 'cutoff_date'
+    _order = 'cutoff_date desc'
+    _inherit = ['mail.thread']
+    _description = 'Account Cut-off'
+
+    @api.multi
+    @api.depends('line_ids')
+    def _compute_total_cutoff(self):
+        for cutoff in self:
+            tamount = 0.0
+            for line in cutoff.line_ids:
+                tamount += line.cutoff_amount
+            cutoff.total_cutoff_amount = tamount
+
+    @api.model
+    def _default_move_label(self):
+        type = self._context.get('type')
+        label = ''
+        if type == 'accrued_expense':
+            label = _('Accrued Expense')
+        elif type == 'accrued_revenue':
+            label = _('Accrued Revenue')
+        elif type == 'prepaid_revenue':
+            label = _('Prepaid Revenue')
+        elif type == 'prepaid_expense':
+            label = _('Prepaid Expense')
+        return label
+
+    @api.model
+    def _inherit_default_cutoff_account_id(self):
+        '''Function designed to be inherited by other cutoff modules'''
+        return None
+
+    @api.model
+    def _default_cutoff_account_id(self):
+        '''This function can't be inherited, so we use a second function'''
+        return self._inherit_default_cutoff_account_id()
+
+    cutoff_date = fields.Date(
+        string='Cut-off Date', readonly=True,
+        states={'draft': [('readonly', False)]}, copy=False,
+        track_visibility='onchange')
+    type = fields.Selection([
+        ('accrued_revenue', 'Accrued Revenue'),
+        ('accrued_expense', 'Accrued Expense'),
+        ('prepaid_revenue', 'Prepaid Revenue'),
+        ('prepaid_expense', 'Prepaid Expense'),
+        ], string='Type', required=True, readonly=True,
+        default=lambda self: self._context.get('type'),
+        states={'draft': [('readonly', False)]})
+    move_id = fields.Many2one(
+        'account.move', string='Cut-off Journal Entry', readonly=True,
+        copy=False)
+    move_label = fields.Char(
+        string='Label of the Cut-off Journal Entry', readonly=True,
+        states={'draft': [('readonly', False)]}, default=_default_move_label,
+        help="This label will be written in the 'Name' field of the "
+        "Cut-off Account Move Lines and in the 'Reference' field of "
+        "the Cut-off Account Move.")
+    cutoff_account_id = fields.Many2one(
+        'account.account', string='Cut-off Account',
+        domain=[('deprecated', '=', False)],
+        readonly=True, states={'draft': [('readonly', False)]},
+        default=_default_cutoff_account_id)
+    cutoff_journal_id = fields.Many2one(
+        'account.journal', string='Cut-off Account Journal',
+        default=lambda self: self.env.user.company_id.
+        default_cutoff_journal_id,
+        readonly=True, states={'draft': [('readonly', False)]})
+    total_cutoff_amount = fields.Monetary(
+        compute='_compute_total_cutoff', string="Total Cut-off Amount",
+        currency_field='company_currency_id',
+        readonly=True, track_visibility='onchange')
+    company_id = fields.Many2one(
+        'res.company', string='Company', required=True, readonly=True,
+        states={'draft': [('readonly', False)]},
+        default=lambda self: self.env['res.company']._company_default_get(
+            'account.cutoff'))
+    company_currency_id = fields.Many2one(
+        related='company_id.currency_id', readonly=True,
+        string='Company Currency')
+    line_ids = fields.One2many(
+        'account.cutoff.line', 'parent_id', string='Cut-off Lines',
+        readonly=True, states={'draft': [('readonly', False)]})
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('done', 'Done'),
+        ], string='State', index=True, readonly=True,
+        track_visibility='onchange', default='draft', copy=False,
+        help="State of the cutoff. When the Journal Entry is created, "
+        "the state is set to 'Done' and the fields become read-only.")
+
+    _sql_constraints = [(
+        'date_type_company_uniq',
+        'unique(cutoff_date, company_id, type)',
+        'A cutoff of the same type already exists with this cut-off date !'
+        )]
+
+    @api.multi
+    def back2draft(self):
+        self.ensure_one()
+        if self.move_id:
+            self.move_id.unlink()
+        self.state = 'draft'
+
+    @api.multi
+    def _prepare_move(self, to_provision):
+        self.ensure_one()
+        movelines_to_create = []
+        amount_total = 0
+        move_label = self.move_label
+        for (cutoff_account_id, analytic_account_id), amount in \
+                to_provision.items():
+            movelines_to_create.append((0, 0, {
+                'account_id': cutoff_account_id,
+                'name': move_label,
+                'debit': amount < 0 and amount * -1 or 0,
+                'credit': amount >= 0 and amount or 0,
+                'analytic_account_id': analytic_account_id,
+            }))
+            amount_total += amount
+
+        # add contre-partie
+        counterpart_amount = amount_total * -1
+        movelines_to_create.append((0, 0, {
+            'account_id': self.cutoff_account_id.id,
+            'name': move_label,
+            'debit': counterpart_amount < 0 and counterpart_amount * -1 or 0,
+            'credit': counterpart_amount >= 0 and counterpart_amount or 0,
+            'analytic_account_id': False,
+        }))
+
+        res = {
+            'journal_id': self.cutoff_journal_id.id,
+            'date': self.cutoff_date,
+            'ref': move_label,
+            'line_ids': movelines_to_create,
+            }
+        return res
+
+    @api.multi
+    def create_move(self):
+        self.ensure_one()
+        move_obj = self.env['account.move']
+        if self.move_id:
+            raise UserError(_(
+                "The Cut-off Journal Entry already exists. You should "
+                "delete it before running this function."))
+        if not self.line_ids:
+            raise UserError(_(
+                "There are no lines on this Cut-off, so we can't create "
+                "a Journal Entry."))
+        to_provision = defaultdict(float)
+        # key = (cutoff_account_id, analytic_account_id)
+        # value = amount
+        for line in self.line_ids:
+            # if it is already present
+            to_provision[(
+                line.cutoff_account_id.id,
+                line.analytic_account_id.id or False
+                )] += line.cutoff_amount
+            # Same for tax lines
+            for tax_line in line.tax_line_ids:
+                to_provision[(
+                    tax_line.cutoff_account_id.id,
+                    tax_line.analytic_account_id.id or False
+                    )] += tax_line.cutoff_amount
+
+        vals = self._prepare_move(to_provision)
+        move = move_obj.create(vals)
+        self.write({'move_id': move.id, 'state': 'done'})
+
+        action = self.env['ir.actions.act_window'].for_xml_id(
+            'account', 'action_move_journal_line')
+        action.update({
+            'view_mode': 'form,tree',
+            'res_id': move.id,
+            'view_id': False,
+            'views': False,
+            })
+        return action
+
+
+class AccountCutoffLine(models.Model):
+    _name = 'account.cutoff.line'
+    _description = 'Account Cut-off Line'
+
+    parent_id = fields.Many2one(
+        'account.cutoff', string='Cut-off', ondelete='cascade')
+    name = fields.Char('Description')
+    company_currency_id = fields.Many2one(
+        related='parent_id.company_currency_id',
+        string="Company Currency", readonly=True)
+    partner_id = fields.Many2one(
+        'res.partner', string='Partner', readonly=True)
+    account_id = fields.Many2one(
+        'account.account', 'Account',
+        domain=[('deprecated', '=', False)], required=True, readonly=True)
+    cutoff_account_id = fields.Many2one(
+        'account.account', string='Cut-off Account',
+        domain=[('deprecated', '=', False)], required=True, readonly=True)
+    cutoff_account_code = fields.Char(
+        related='cutoff_account_id.code',
+        string='Cut-off Account Code', readonly=True)
+    analytic_account_id = fields.Many2one(
+        'account.analytic.account', string='Analytic Account',
+        domain=[('account_type', '!=', 'closed')], readonly=True)
+    analytic_account_code = fields.Char(
+        related='analytic_account_id.code',
+        string='Analytic Account Code', readonly=True)
+    currency_id = fields.Many2one(
+        'res.currency', string='Amount Currency', readonly=True,
+        help="Currency of the 'Amount' field.")
+    amount = fields.Monetary(
+        string='Amount', currency_field='currency_id', readonly=True,
+        help="Amount that is used as base to compute the Cut-off Amount. "
+        "This Amount is in the 'Amount Currency', which may be different "
+        "from the 'Company Currency'.")
+    cutoff_amount = fields.Monetary(
+        string='Cut-off Amount', currency_field='company_currency_id',
+        readonly=True,
+        help="Cut-off Amount without taxes in the Company Currency.")
+    tax_ids = fields.Many2many(
+        'account.tax', id1='cutoff_line_id', id2='tax_id', string='Taxes',
+        readonly=True)
+    tax_line_ids = fields.One2many(
+        'account.cutoff.tax.line', 'parent_id', string='Cut-off Tax Lines',
+        readonly=True)
+
+
+class AccountCutoffTaxLine(models.Model):
+    _name = 'account.cutoff.tax.line'
+    _description = 'Account Cut-off Tax Line'
+
+    parent_id = fields.Many2one(
+        'account.cutoff.line', string='Account Cut-off Line',
+        ondelete='cascade', required=True)
+    tax_id = fields.Many2one('account.tax', string='Tax', required=True)
+    cutoff_account_id = fields.Many2one(
+        'account.account', string='Cut-off Account',
+        domain=[('deprecated', '=', False)], required=True, readonly=True)
+    analytic_account_id = fields.Many2one(
+        'account.analytic.account', string='Analytic Account',
+        domain=[('account_type', '!=', 'closed')], readonly=True)
+    base = fields.Monetary(
+        string='Base', currency_field='currency_id',
+        readonly=True, help="Base Amount in the currency of the PO.")
+    amount = fields.Monetary(
+        string='Tax Amount', currency_field='currency_id',
+        readonly=True, help='Tax Amount in the currency of the PO.')
+    sequence = fields.Integer(string='Sequence', readonly=True)
+    cutoff_amount = fields.Monetary(
+        string='Cut-off Tax Amount', currency_field='company_currency_id',
+        readonly=True, help="Tax Cut-off Amount in the company currency.")
+    currency_id = fields.Many2one(
+        related='parent_id.currency_id', string='Currency', readonly=True)
+    company_currency_id = fields.Many2one(
+        related='parent_id.company_currency_id',
+        string="Company Currency", readonly=True)
+
+
+class AccountCutoffMapping(models.Model):
+    _name = 'account.cutoff.mapping'
+    _description = 'Account Cut-off Mapping'
+    _rec_name = 'account_id'
+
+    company_id = fields.Many2one(
+        'res.company', string='Company', required=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+            'account.cutoff.mapping'))
+    account_id = fields.Many2one(
+        'account.account', string='Regular Account',
+        domain=[('deprecated', '=', False)], required=True)
+    cutoff_account_id = fields.Many2one(
+        'account.account', string='Cut-off Account',
+        domain=[('deprecated', '=', False)], required=True)
+    cutoff_type = fields.Selection([
+        ('all', 'All Cut-off Types'),
+        ('accrued_revenue', 'Accrued Revenue'),
+        ('accrued_expense', 'Accrued Expense'),
+        ('prepaid_revenue', 'Prepaid Revenue'),
+        ('prepaid_expense', 'Prepaid Expense'),
+        ], string='Cut-off Type', required=True)
+
+    @api.model
+    def _get_mapping_dict(self, company_id, cutoff_type='all'):
+        '''return a dict with:
+        key = ID of account,
+        value = ID of cutoff_account'''
+        if cutoff_type == 'all':
+            cutoff_type_filter = ('all')
+        else:
+            cutoff_type_filter = ('all', cutoff_type)
+        mappings = self.search([
+            ('company_id', '=', company_id),
+            ('cutoff_type', 'in', cutoff_type_filter),
+            ])
+        mapping = {}
+        for item in mappings:
+            mapping[item.account_id.id] = item.cutoff_account_id.id
+        return mapping
