4b84bc3769c611806abdb1792b887cacb41e6c99
Alexis de Lattre <alexis.delattre@akretion.com>
2016-07-25 01:03:07 +0200

    Port account_cutoff_base and account_cutoff_prepaid to Odoo v9
    Extract a new module account_invoice_start_end_dates from account_cutoff_prepaid

---
+++ account_cutoff_prepaid/models/company.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    default_prepaid_revenue_account_id = fields.Many2one(
+        'account.account', string='Default Account for Prepaid Revenue',
+        domain=[('deprecated', '=', False)])
+    default_prepaid_expense_account_id = fields.Many2one(
+        'account.account', string='Default Account for Prepaid Expense',
+        domain=[('deprecated', '=', False)])
