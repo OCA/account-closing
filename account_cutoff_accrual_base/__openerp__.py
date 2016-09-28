b5671c106c073ba965f569662204069c91b0cc5d
jcoux <julien.coux@camptocamp.com>
2016-09-28 14:20:55 +0200

    Update __openerp__.py and README.rst to OCA standard

--- account_cutoff_accrual_base/__openerp__.py
+++ account_cutoff_accrual_base/__openerp__.py
@@ -8,13 +8,7 @@
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Base module for accrued expenses and revenues',
-    'description': """This module contains objets, fields and menu entries that are used by other accrual modules. So you need to install other accrual modules to get the additionnal functionalities :
-- the module 'account_cutoff_accrual_picking' will manage accrued expenses and revenues based on pickings.
-- a not-developped-yet module will manage accrued expenses and revenues based on timesheets.
-
-This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
-    """,
-    'author': 'Akretion',
+    'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com',
     'depends': ['account_cutoff_base'],
     'data': [
--- account_cutoff_accrual_picking/__openerp__.py
+++ account_cutoff_accrual_picking/__openerp__.py
@@ -4,28 +4,11 @@
 
 {
     'name': 'Account Accrual Picking',
-    'version': '0.1',
+    'version': '8.0.0.1.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Accrued Expense & Accrued Revenue from Pickings',
-    'description': """
-Manage expense and revenue accruals from pickings
-=================================================
-
-This module generates expense and revenue accruals based on the status of pickings.
-
-For revenue accruals, Odoo will take into account all the delivery orders in *Delivered* state that have been shipped before the cut-off date with *Invoice Control* = *Invoiced* with an invoice date after the cut-off date or *Invoice Control* = *To Be Invoiced*.
-
-For expense accruals, OpenERP will take into account all the incoming shipments in *Received* state that have been received before the cut-off date with *Invoice Control* = *Invoiced* with an invoice date after the cut-off date or *Invoice Control* = *To Be Invoiced*.
-
-The current code of the module only works when:
-
-* on sale orders, the *Create Invoice* field is set to *On Delivery Order* ;
-* for purchase orders, the *Invoicing Control* field is set to *Based on incoming shipments*.
-
-This module has been written by Alexis de Lattre from Akretion <alexis.delattre@akretion.com>.
-    """,
-    'author': 'Akretion',
+    'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': [
         'account_cutoff_accrual_base',
