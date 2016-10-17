1c10d2e367e2674e15e7f041c0fdd7881e35951a
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-17 11:49:44 +0200

    Migration of account_cutoff modules to 10.0

--- account_cutoff_prepaid/__manifest__.py
+++ account_cutoff_prepaid/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Account Cut-off Prepaid',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Prepaid Expense, Prepaid Revenue',
