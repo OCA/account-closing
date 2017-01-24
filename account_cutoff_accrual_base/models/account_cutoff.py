bb372d9cae2a1a03d72c322881516be341b9df1c
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-01-24 15:03:18 +0100

    Fix flake8

--- account_cutoff_accrual_base/models/account_cutoff.py
+++ account_cutoff_accrual_base/models/account_cutoff.py
@@ -44,7 +44,7 @@ class AccountCutOff(models.Model):
     def _get_default_journal(self):
         journal_id = super(AccountCutOff, self)\
             ._get_default_journal()
-        cutoff_type = context.get('type', False)
+        cutoff_type = self.env.context.get('type', False)
         default_journal_id = self.env.user.company_id\
             .default_cutoff_journal_id.id or False
         if cutoff_type == 'accrued_expense':
