c16e719e513bf51b7ff78fc684b04255a0801016
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-02-22 18:46:48 +0100

    [MIG] account_multicurrency_revaluation_report to 10.0

--- account_multicurrency_revaluation_report/report/currency_unrealized_report.py
+++ account_multicurrency_revaluation_report/report/currency_unrealized_report.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from openerp.report import report_sxw
-from openerp.tools.translate import _
-from openerp import pooler, models
+from odoo import api, models
 
 
 class ShellAccount(object):
@@ -31,25 +13,12 @@ class ShellAccount(object):
     # beacause of function fields
     # This object aim to be easly transpose to account account if needed
 
-    def exists(self):
-        return True
-
-    def __init__(self, cr, uid, pool, acc_id, context=None):
-        self._context = context or {}
-        self.cursor = cr
-        self.uid = uid
-        self.acc_id = acc_id
-        self.pool = pool
-        tmp = self.pool['account.account'].read(
-            cr, uid, [acc_id], ['id', 'name', 'code', 'currency_revaluation'],
-            self._context)
-        self.name = tmp[0].get('name')
-        self.code = tmp[0].get('code')
+    def __init__(self, account):
+        self.cursor = account.env.cr
+        tmp = account.read(
+            ['id', 'name', 'code', 'currency_revaluation'])
         self.account_id = tmp[0]['id']
         self.ordered_lines = []
-        self.company_id = self.pool['res.users'].browse(
-            cr, uid, uid, context=context).company_id
-        self.currency_revaluation = tmp[0].get('currency_revaluation', False)
         self.keys_to_sum = ['gl_foreign_balance', 'gl_currency_rate',
                             'gl_revaluated_balance', 'gl_balance',
                             'gl_ytd_balance']
@@ -97,58 +66,30 @@ class ShellAccount(object):
             setattr(self, key + '_total', val)
 
 
-class CurrencyUnrealizedReport(report_sxw.rml_parse):
+class CurrencyUnrealizedReport(models.AbstractModel):
+    _name = 'report.account_multicurrency_revaluation_report.curr_unrealized'
 
-    def __init__(self, cursor, uid, name, context):
-        super(CurrencyUnrealizedReport, self).__init__(
-            cursor, uid, name, context=context)
-
-        self.pool = pooler.get_pool(self.cr.dbname)
-        self.cursor = self.cr
-        self.uid = uid
-        self.company = self.pool.get('res.users').browse(
-            self.cr, uid, uid, context=context).company_id
-        self.localcontext.update({
-            'report_name': _('Exchange Rate Gain and Loss Report'),
-        })
-
-    def set_context(self, objects, data, ids, report_type=None):
-        """Populate a ledger_lines attribute on each browse record that will
-        be used by mako template.
-        """
-        # we replace object
-        objects = []
-
-        # Redefine data['form'] so we can call report as HTML without
-        # wizard on          https://root_server_address/report/html/
-        # account_multicurrency_revaluation_report.curr_unrealized
-        if not data.get('form'):
-            data['form'] = {}
-            data['form']['account_ids'] = self.pool('account.account').search(
-                self.cr, self.uid, [('currency_revaluation', '=', True)])
-            data['lang'] = 'en_US'
-            data['uid'] = self.uid
-            data['params'] = {}
-
-        accounts = self.pool.get('account.account').browse(
-            self.cr, self.uid, data['form'].get('account_ids')).sorted()
-
+    @api.model
+    def render_html(self, docids, data=None):
+        shell_accounts= {}
+        docs = self.env['account.account']
+        data = data if data is not None else {}
+        accounts = self.env['account.account'].search(
+            [('currency_revaluation', '=', True)])
         for account in accounts:
-            acc = ShellAccount(
-                self.cursor, self.uid, self.pool, account.id,
-                context=self.localcontext)
-            if not acc.currency_revaluation:
-                continue
+            acc = ShellAccount(account)
             acc.get_lines()
             if acc.ordered_lines:
-                objects.append(acc)
+                docs |= account
+                shell_accounts[account.id] = acc
                 acc.compute_totals()
-        return super(CurrencyUnrealizedReport, self).set_context(
-            objects, data, ids, report_type=None)
-
-
-class ReportPrintCurrencyUnrealized(models.AbstractModel):
-    _name = 'report.account_multicurrency_revaluation_report.curr_unrealized'
-    _inherit = 'report.abstract_report'
-    _template = 'account_multicurrency_revaluation_report.curr_unrealized'
-    _wrapped_report_class = CurrencyUnrealizedReport
+        docargs = {
+            'doc_ids': docs.ids,
+            'doc_model': 'account.account',
+            'docs': docs,
+            'shell_accounts': shell_accounts,
+            'data': dict(
+                data,
+            ),
+        }
+        return self.env['report'].render(self._name[7:], docargs)
