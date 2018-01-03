bc8b8b2b7c83d65f1a17d69ea7ef5379cf7fa00e
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-01-03 17:21:54 +0200

    [MIG] account_cutoff_base: Migration to 11.0

--- account_cutoff_base/__manifest__.py
+++ account_cutoff_base/__manifest__.py
@@ -1,16 +1,18 @@
-# -*- coding: utf-8 -*-
-# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2013-2016 Akretion
+# Copyright 2018 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Cut-off Base',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Base module for Account Cut-offs',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'depends': ['account_accountant'],
+    'website': 'https://github.com/OCA/account-closing',
+    'depends': [
+        'account',
+    ],
     'data': [
         'security/account_cutoff_base_security.xml',
         'security/ir.model.access.csv',
