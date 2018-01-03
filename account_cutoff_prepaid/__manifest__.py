0bd1787e0785f9ad6276ef8e63ab2b7ffaa65194
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-01-03 14:51:49 +0200

    [MIG] account_cutoff_prepaid: Migration to 11.0

--- account_cutoff_prepaid/__manifest__.py
+++ account_cutoff_prepaid/__manifest__.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
-# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018 CampToCamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Cut-off Prepaid',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Prepaid Expense, Prepaid Revenue',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/account-closing',
     'depends': [
         'account_cutoff_base',
         'account_invoice_start_end_dates',
-        ],
+    ],
     'data': [
         'views/company.xml',
         'views/account_cutoff.xml',
@@ -22,6 +22,6 @@
         'images/prepaid_revenue_draft.jpg',
         'images/prepaid_revenue_journal_entry.jpg',
         'images/prepaid_revenue_done.jpg',
-        ],
+    ],
     'installable': True,
 }
--- account_cutoff_prepaid/models/__init__.py
+++ account_cutoff_prepaid/models/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import company
 from . import account_cutoff
