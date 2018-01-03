9182ee1849d2d1a6d25a9666990c0536ff34acb2
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-01-03 20:48:44 +0200

    account_invoice_start_end_dates: Migration to 11.0

--- account_invoice_start_end_dates/__manifest__.py
+++ account_invoice_start_end_dates/__manifest__.py
@@ -1,16 +1,18 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2018 CampToCamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Start End Dates',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Adds start/end dates on invoice lines and move lines',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
-    'depends': ['account'],
+    'website': 'https://github.com/OCA/account-closing',
+    'depends': [
+        'account',
+    ],
     'data': [
         'views/account_invoice.xml',
         'views/account_move.xml',
