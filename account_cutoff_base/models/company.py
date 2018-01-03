4145b1b67d52050a20d1c223f98116ecb7fdcd40
oleksandrpaziuk <o.paziuk@mobilunity.com>
2018-01-03 17:21:54 +0200

    [MIG] account_cutoff_base: Migration to 11.0

--- account_cutoff_base/models/company.py
+++ account_cutoff_base/models/company.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2013-2016 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models, fields
--- account_cutoff_base/tests/__init__.py
+++ account_cutoff_base/tests/__init__.py
@@ -1,6 +1,1 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-
 from . import test_account_cutoff
