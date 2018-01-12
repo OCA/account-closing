d8f62f778633c5ed6abcbb30d17ddb258ec8d8e3
Patrick Tombez <patrick.tombez@camptocamp.com>
2018-01-12 12:09:39 +0100

    * Fixup Add oca_dependencies.txt * Fix according to replacement of proxied default cutoff account id * s/openerp/odoo/ in imports * Replace headers with shorter one

--- account_cutoff_accrual_base/models/account_cutoff.py
+++ account_cutoff_accrual_base/models/account_cutoff.py
@@ -1,35 +1,18 @@
-##############################################################################
-#
-#    Account Cut-off Accrual Base module for OpenERP
-#    Copyright (C) 2013 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
 
 
-from openerp import api, fields, models
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
 
 
 class AccountCutOff(models.Model):
     _inherit = 'account.cutoff'
 
     @api.model
-    def _inherit_default_cutoff_account_id(self):
-        account_id = super()._inherit_default_cutoff_account_id()
+    def _default_cutoff_account_id(self):
+        account_id = super()._default_cutoff_account_id()
         type = self.env.context.get('type')
         company = self.env.user.company_id
         if type == 'accrued_expense':
