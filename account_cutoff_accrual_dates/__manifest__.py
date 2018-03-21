2fd8a0cdd9d33ac2bea74733f0bc683d075e5778
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-21 01:08:27 +0100

    Add module account_cutoff_accrual_dates
    Allow use of the cutoff modules by Accountant (and not only Financial manager)
    Set default cutoff date to last day of previous fiscal year
    Move button to generate lines to account_cutoff_base module
    Block deletion of 'done' cutoff
    Use accounting configuration page instead of company form view
    Code clean-up and minor fixes

---
+++ account_cutoff_accrual_dates/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion France
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Accrual Dates',
+    'version': '10.0.1.1.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'summary': 'Accrued expenses and revenues based on start/end dates',
+    'author': "Akretion,Odoo Community Association (OCA)",
+    'website': 'http://www.akretion.com',
+    'depends': ['account_cutoff_prepaid', 'account_cutoff_accrual_base'],
+    'data': [],
+    'installable': True,
+}
