dc790b9296518f8c4f39ad15239acf15d962e9ae
Alexis de Lattre <alexis.delattre@akretion.com>
2018-03-21 01:08:27 +0100

    Add module account_cutoff_accrual_dates
    Allow use of the cutoff modules by Accountant (and not only Financial manager)
    Set default cutoff date to last day of previous fiscal year
    Move button to generate lines to account_cutoff_base module
    Block deletion of 'done' cutoff
    Use accounting configuration page instead of company form view
    Code clean-up and minor fixes

--- account_cutoff_accrual_base/__manifest__.py
+++ account_cutoff_accrual_base/__manifest__.py
@@ -1,38 +1,20 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Account Cutoff Accrual Base module for OpenERP
-#    Copyright (C) 2013 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# Copyright 2013-2018 Akretion
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Accrual Base',
-    'version': '10.0.0.1.0',
-    'category': 'Accounting & Finance',
+    'version': '10.0.1.1.0',
+    'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Base module for accrued expenses and revenues',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'http://www.akretion.com',
     'depends': ['account_cutoff_base'],
     'data': [
-        'views/company_view.xml',
-        'views/account_view.xml',
+        'views/account_config_settings.xml',
+        'views/account_tax.xml',
         'views/account_cutoff_view.xml',
     ],
     'installable': True,
