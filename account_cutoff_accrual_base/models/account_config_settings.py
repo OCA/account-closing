57cc5bfd688760e12c1477e137066eb64fc05846
Jacques-Etienne Baudoux <je@bcim.be>
2018-06-14 16:48:03 +0200

    fix after rebase; moved accrual_taxes to base accrual module; fixed journal and number for accrual move and reversal

--- account_cutoff_accrual_base/models/account_config_settings.py
+++ account_cutoff_accrual_base/models/account_config_settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
@@ -17,3 +18,5 @@ class AccountConfigSettings(models.TransientModel):
         related='company_id.default_accrual_revenue_journal_id')
     default_accrual_expense_journal_id = fields.Many2one(
         related='company_id.default_accrual_expense_journal_id')
+    accrual_taxes = fields.Boolean(
+        related='company_id.accrual_taxes')
