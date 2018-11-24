51d13ff6637c229dd295e613103cf3ba4968364d
Humberto Arocha <hbto@vauxoo.com>
2018-11-24 14:03:22 +0000

    [MIG] account_multicurrency_revaluation: Migration to 12.0
    - getting rid of validation because rate will always be at least '1.0' because
    of (res.currency)_get_rates query
    
    - create just one Journal Entry per Currency Revaluation run depending the
    account type

--- account_multicurrency_revaluation/tests/test_currency_revaluation.py
+++ account_multicurrency_revaluation/tests/test_currency_revaluation.py
@@ -1,9 +1,9 @@
 # Copyright 2012-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from datetime import timedelta
 from odoo.tests.common import SavepointCase
 from odoo import fields
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
 
 
 class TestCurrencyRevaluation(SavepointCase):
@@ -58,8 +58,7 @@ class TestCurrencyRevaluation(SavepointCase):
 
         payment_term = ref('account.account_payment_term')
 
-        year = fields.Date.from_string(fields.Date.today()).strftime('%Y')
-
+        year = fields.Date.today().strftime('%Y')
         invoice = cls.env['account.invoice'].create({
             'name': "Customer Invoice",
             'date_invoice': '%s-01-16' % year,
@@ -151,15 +150,15 @@ class TestCurrencyRevaluation(SavepointCase):
             'revaluation_gain_account_id':
                 self.env.ref('account_multicurrency_revaluation.'
                              'acc_reval_gain').id,
-            'default_currency_reval_journal_id': self.reval_journal.id,
+            'currency_reval_journal_id': self.reval_journal.id,
         }
         self.company.write(values)
         self.assertEqual(self.company.currency_id, self.env.ref('base.EUR'))
 
         wizard = self.env['wizard.currency.revaluation']
         data = {
-            'revaluation_date': '%s-03-15' % fields.Date.from_string(
-                fields.Date.today()).strftime('%Y'),
+            'revaluation_date': '%s-03-15' % fields.Date.today().strftime(
+                '%Y'),
             'journal_id': self.reval_journal.id,
             'label': '[%(account)s] wiz_test',
         }
@@ -206,8 +205,260 @@ class TestCurrencyRevaluation(SavepointCase):
                              'acc_reval_gain').id
         }).execute()
 
-        wizard = self.env['wizard.currency.revaluation'].create({})
+        wizard = self.env['wizard.currency.revaluation'].create(
+            {'journal_id': self.reval_journal.id, })
 
-        self.assertEqual(wizard.revaluation_date,
-                         fields.date.today().strftime(DATE_FORMAT))
+        self.assertEqual(wizard.revaluation_date, fields.date.today())
         self.assertEqual(wizard.journal_id, self.reval_journal)
+
+    def test_us_revaluation(self):
+        """Create Invoices and Run the revaluation currency wizard
+        with different rates which result should be:
+                                 Debit    Credit    Amount Currency
+        Customer Invoice US      25.00    0.00      100.00
+        Customer Invoice US      40.00    0.00      100.00
+
+        Fisrt wizard execution:
+
+            Currency Reval. 1.0  135.00   0.00    0.00
+
+        Second wizard execution:
+
+            Currency Reval 1.25    0.00   40.00     0.00
+        """
+        self.delete_journal_data()
+        usd_currency = self.env.ref('base.USD')
+        dates = (fields.Date.today() - timedelta(days=30),
+                 fields.Date.today() - timedelta(days=15),
+                 fields.Date.today() - timedelta(days=7),
+                 fields.Date.today() - timedelta(days=1),
+                 )
+        rates = (4.00, 2.50, 1.00, 1.25)
+        self.create_rates(dates, rates, usd_currency)
+        acc_reval_loss = self.env.ref('account_multicurrency_revaluation.'
+                                      'acc_reval_loss')
+        acc_reval_gain = self.env.ref('account_multicurrency_revaluation.'
+                                      'acc_reval_gain')
+        values = {
+            'revaluation_loss_account_id': acc_reval_loss.id,
+            'revaluation_gain_account_id': acc_reval_gain.id,
+            'currency_reval_journal_id': self.reval_journal.id,
+        }
+        self.company.write(values)
+        receivable_acc = self.env.ref(
+            'account_multicurrency_revaluation.demo_acc_receivable')
+        receivable_acc.write({'reconcile': True})
+
+        invoice1 = self.create_invoice(
+            fields.Date.today() - timedelta(days=30), usd_currency, 1.0,
+            100.00)
+        invoice1.action_invoice_open()
+        invoice2 = self.create_invoice(
+            fields.Date.today() - timedelta(days=15), usd_currency, 1.0,
+            100.00)
+        invoice2.action_invoice_open()
+
+        reval_move_lines = self.env['account.move.line'].search([
+            ('account_id', '=', receivable_acc.id)])
+        self.assertEqual(sum(reval_move_lines.mapped('debit')), 65.00)
+        self.assertEqual(sum(reval_move_lines.mapped('amount_currency')),
+                         200.00)
+
+        result = self.wizard_execute(fields.Date.today() - timedelta(days=7))
+        self.assertEqual(result.get('name'), "Created revaluation lines")
+        reval_move_lines = self.env['account.move.line'].search([
+            ('account_id', '=', receivable_acc.id)])
+        self.assertEqual(sum(reval_move_lines.mapped('debit')), 200.00)
+        self.assertEqual(sum(reval_move_lines.mapped('amount_currency')),
+                         200.00)
+
+        result = self.wizard_execute(fields.Date.today() - timedelta(days=1))
+        self.assertEqual(result.get('name'), "Created revaluation lines")
+        reval_move_lines = self.env['account.move.line'].search([
+            ('account_id', '=', receivable_acc.id)])
+        self.assertEqual(sum(reval_move_lines.mapped('debit')), 200.00)
+        self.assertEqual(sum(reval_move_lines.mapped('credit')), 40.00)
+        self.assertEqual(sum(reval_move_lines.mapped('amount_currency')),
+                         200.00)
+
+        wizard = self.env['unrealized.report.printer']
+        wiz = wizard.create({})
+        result = wiz.print_report(data={})
+        account_ids = result.get('context').get('active_ids')
+        report = self.env['account.move.line'].search(
+            [('account_id', 'in', account_ids)]).filtered(
+                lambda l: l.account_id.code == 'accrec')
+        self.assertEqual(sum(report.mapped('debit')), 200)
+        self.assertEqual(sum(report.mapped('credit')), 40)
+        self.assertEqual(sum(report.mapped('amount_currency')), 200)
+
+    def test_revaluation_payment(self):
+        """Create an Invoice and execute the revaluation currency wizard with
+        a rate of 0.75:
+                                    Debit    Credit       Amount Currency
+        Account Receivable
+          currency revaluation      0.00     2666.67      0.00
+          customer invoices         6666.67  0.00         5000.00
+
+        Make a payment with 1.0 rate and execute the revaluation currency
+        wizard with a rate of 1.25:
+
+                                    Debit     Credit      Amount Currency
+        Account Receivable
+          currency revaluation      0.00      2666.67     0.00
+          currency revaluation      0.00      800.00      0.00
+          euro bank                 0.00      4000.00    -4000.00
+          customer invoices         6666.67   0.00        5000.00
+
+        """
+        self.delete_journal_data()
+        usd_currency = self.env.ref('base.USD')
+        eur_currency = self.env.ref('base.EUR')
+        year = fields.Date.today().strftime('%Y')
+        dates = (fields.Date.to_date('%s-11-10' % year),
+                 fields.Date.to_date('%s-11-14' % year),
+                 fields.Date.to_date('%s-11-16' % year),
+                 )
+        rates = (0.75, 1.00, 1.25)
+        self.create_rates(dates, rates, eur_currency)
+        acc_reval_loss = self.env.ref('account_multicurrency_revaluation.'
+                                      'acc_reval_loss')
+        acc_reval_gain = self.env.ref('account_multicurrency_revaluation.'
+                                      'acc_reval_gain')
+        values = {
+            'revaluation_loss_account_id': acc_reval_loss.id,
+            'revaluation_gain_account_id': acc_reval_gain.id,
+            'currency_reval_journal_id': self.reval_journal.id,
+            'currency_id': usd_currency.id,
+        }
+        self.company.write(values)
+        receivable_acc = self.env.ref(
+            'account_multicurrency_revaluation.demo_acc_receivable')
+
+        invoice = self.create_invoice(fields.Date.to_date('%s-11-11' % year),
+                                      eur_currency, 5.0, 1000.00)
+        invoice.action_invoice_open()
+        result = self.wizard_execute(fields.Date.to_date('%s-11-16' % year))
+        self.assertEqual(result.get('name'), "Created revaluation lines")
+        reval_move_lines = self.env['account.move.line'].search([
+            ('account_id', '=', receivable_acc.id)])
+        self.assertEqual(sum(reval_move_lines.mapped('debit')), 6666.67)
+        self.assertEqual(sum(reval_move_lines.mapped('credit')), 2666.67)
+        self.assertEqual(sum(reval_move_lines.mapped('amount_currency')),
+                         5000.00)
+
+        euro_bank = self.env['account.journal'].create({
+            'name': 'Euro Bank',
+            'type': 'bank',
+            'code': 'EURBK',
+            'currency_id': eur_currency.id,
+        })
+        euro_bank.default_debit_account_id.currency_revaluation = True
+        payment_method = self.env.ref('account_multicurrency_revaluation.'
+                                      'account_payment_method_manual_in')
+
+        # Register partial payment
+        payment = self.env['account.payment'].create({
+            'invoice_ids': [(4, invoice.id, 0)],
+            'amount': 4000,
+            'currency_id': eur_currency.id,
+            'payment_date': '%s-11-15' % year,
+            'communication': 'Invoice partial payment',
+            'partner_id': invoice.partner_id.id,
+            'partner_type': 'customer',
+            'journal_id': euro_bank.id,
+            'payment_type': 'inbound',
+            'payment_method_id': payment_method.id,
+            'payment_difference_handling': 'open',
+            'writeoff_account_id': False,
+        })
+        payment.post()
+
+        result = self.wizard_execute(fields.Date.to_date('%s-11-16' % year))
+        self.assertEqual(result.get('name'), "Created revaluation lines")
+        reval_move_lines = self.env['account.move.line'].search([
+            ('account_id', '=', receivable_acc.id)])
+        self.assertEqual(sum(reval_move_lines.mapped('debit')), 7466.67)
+        self.assertEqual(sum(reval_move_lines.mapped('credit')), 6666.67)
+        self.assertEqual(sum(reval_move_lines.mapped('amount_currency')),
+                         1000.00)
+
+        receivable_lines = len(reval_move_lines)
+        result = self.wizard_execute(fields.Date.to_date('%s-11-16' % year))
+        reval_move_lines = self.env['account.move.line'].search([
+            ('account_id', '=', receivable_acc.id)])
+        self.assertEqual(len(reval_move_lines), receivable_lines)
+
+    def create_rates(self, dates, rates, currency):
+        self.env['res.currency.rate'].search([]).unlink()
+        for date, rate in zip(dates, rates):
+            self.env['res.currency.rate'].create({
+                'currency_id': currency.id,
+                'company_id': self.company.id,
+                'name': date,
+                'rate': rate
+            })
+
+    def create_invoice(self, date, currency, quantity, price):
+        receivable_acc = self.env.ref(
+            'account_multicurrency_revaluation.demo_acc_receivable')
+        receivable_acc.write({'reconcile': True})
+        revenue_acc = self.env.ref('account_multicurrency_revaluation.'
+                                   'demo_acc_revenue')
+        payment_term = self.env.ref('account.account_payment_term')
+        sales_journal = self.env.ref(
+            'account_multicurrency_revaluation.sales_journal')
+        partner = self.env.ref('base.res_partner_3')
+        partner.company_id = self.company.id
+        partner.property_account_payable_id = receivable_acc.id
+
+        invoice_line_data = {
+            'product_id': self.env.ref('product.product_product_5').id,
+            'quantity': quantity,
+            'account_id': revenue_acc.id,
+            'name': 'product test 5',
+            'price_unit': price,
+            'currency_id': currency.id,
+        }
+        invoice = self.env['account.invoice'].create({
+            'name': "Customer Invoice",
+            'date_invoice': date,
+            'currency_id': currency.id,
+            'journal_id': sales_journal.id,
+            'company_id': self.company.id,
+            'partner_id': partner.id,
+            'account_id': receivable_acc.id,
+            'invoice_line_ids': [(0, 0, invoice_line_data)],
+            'payment_term_id': payment_term.id,
+        })
+        return invoice
+
+    def wizard_execute(self, date):
+        data = {
+            'revaluation_date': date,
+            'journal_id': self.reval_journal.id,
+            'label': '[%(account)s] [%(currency)s] wiz_test',
+        }
+        wiz = self.env['wizard.currency.revaluation'].create(data)
+        return wiz.revaluate_currency()
+
+    def delete_journal_data(self):
+        """Delete journal data
+        delete all journal-related data, so a new currency can be set.
+        """
+
+        company = self.company
+        moves = self.env['account.move'].search(
+            [('company_id', '=', company.id)])
+        moves.write({'state': 'draft'})
+        invoices = self.env['account.invoice'].search(
+            [('company_id', '=', company.id)])
+        invoices.write({'state': 'draft', 'move_name': False})
+
+        models_to_clear = [
+            'account.move.line', 'account.invoice', 'account.bank.statement']
+        for model in models_to_clear:
+            records = self.env[model].search([('company_id', '=', company.id)])
+            if model == 'account.move.line':
+                records.remove_move_reconcile()
+            records.unlink()
