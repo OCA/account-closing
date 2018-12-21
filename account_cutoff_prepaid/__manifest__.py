9ddbb8ba49426d50e4653ccd051f566b6f2a5b63
Jim Hoefnagels <jim.hoefnagels@dynapps.be>
2018-12-21 13:32:59 +0100

    [MIG] account_cutoff_prepaid: Migration to 12.0

--- account_cutoff_prepaid/__manifest__.py
+++ account_cutoff_prepaid/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Account Cut-off Prepaid',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Prepaid Expense, Prepaid Revenue',
