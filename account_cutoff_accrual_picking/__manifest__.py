77a46fd2b75055dbec4340b5ba574238c11a6f80
Alexis de Lattre <alexis.delattre@akretion.com>
2019-05-08 00:00:57 +0200

    [MIG] account_cutoff_accrual_base, account_cutoff_accrual_dates and account_cutoff_accrual_picking from v10 to v12
    account_cutoff_base and account_cutoff_prepaid: Up-port improvements from v10 branch

--- account_cutoff_accrual_picking/__manifest__.py
+++ account_cutoff_accrual_picking/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
-# Copyright 2013-2018 Akretion France (http://www.akretion.com)
+# Copyright 2013-2019 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Cut-off Accrual Picking',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
-    'summary': 'Accrued Expense & Accrued Revenue from Pickings',
+    'summary': 'Accrued expense & accrued revenue from pickings',
     'author': "Akretion,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/account-closing',
     'depends': ['account_cutoff_accrual_base', 'purchase', 'sale_stock'],
