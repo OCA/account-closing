c824bd5db8e4ae0a29e358e01a97940285c2c415
sbejaoui <souheil.bejaoui@acsone.eu>
2019-05-21 11:51:12 +0200

    [ADD] - Add missing fields

--- account_cutoff_accrual_base/models/company.py
+++ account_cutoff_accrual_base/models/company.py
@@ -15,9 +15,27 @@ class ResCompany(models.Model):
         string='Default Account for Accrued Revenues',
         domain=[('deprecated', '=', False)])
 
+    default_accrued_revenue_return_account_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Default Account for Accrued Revenues Returns',
+        domain=[('deprecated', '=', False)])
+
     default_accrued_expense_account_id = fields.Many2one(
         comodel_name='account.account',
         string='Default Account for Accrued Expenses',
         domain=[('deprecated', '=', False)])
+
+    default_accrued_expense_return_account_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Default Account for Accrued Expenses Returns',
+        domain=[('deprecated', '=', False)])
+
+    default_accrual_revenue_journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        string='Default Journal for Accrued Revenues')
+
+    default_accrual_expense_journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        string='Default Journal for Accrued Expenses')
 
     accrual_taxes = fields.Boolean(string='Accrual On Taxes')
