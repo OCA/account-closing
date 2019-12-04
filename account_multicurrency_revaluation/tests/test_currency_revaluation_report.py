7a8447485e0c5695e290ac0ad342d6ccde282ade
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-12-04 11:51:09 -0500

    [IMP] account_multicurrency_revaluation: black, isort

--- account_multicurrency_revaluation/tests/test_currency_revaluation_report.py
+++ account_multicurrency_revaluation/tests/test_currency_revaluation_report.py
@@ -5,16 +5,14 @@ from odoo.tests.common import TransactionCase
 
 
 class TestCurrencyRevaluationReport(TransactionCase):
-
     def test_wizard_empty_accounts(self):
-        wizard = self.env['unrealized.report.printer']
+        wizard = self.env["unrealized.report.printer"]
         wiz = wizard.create({})
-        data = {'lang': 'en_US',
-                'tz': 'Europe/Brussels',
-                'uid': 1}
+        data = {"lang": "en_US", "tz": "Europe/Brussels", "uid": 1}
         result = wiz.print_report(data)
 
-        self.assertEqual(result.get('type'), "ir.actions.report")
+        self.assertEqual(result.get("type"), "ir.actions.report")
         self.assertEqual(
-            result.get('report_name'),
-            "account_multicurrency_revaluation.curr_unrealized_report")
+            result.get("report_name"),
+            "account_multicurrency_revaluation.curr_unrealized_report",
+        )
