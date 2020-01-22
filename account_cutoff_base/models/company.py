31d3cf59847b9ed316400517af102c5367eaf86a
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-01-22 11:48:10 +0100

    [IMP] account_cutoff_base: black, isort

--- account_cutoff_base/models/company.py
+++ account_cutoff_base/models/company.py
@@ -1,11 +1,12 @@
 # Copyright 2013-2016 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     default_cutoff_journal_id = fields.Many2one(
-        'account.journal', string='Default Cut-off Journal')
+        "account.journal", string="Default Cut-off Journal"
+    )
--- account_cutoff_base/tests/test_account_cutoff.py
+++ account_cutoff_base/tests/test_account_cutoff.py
@@ -4,9 +4,7 @@ from odoo.tests.common import SavepointCase
 
 
 class TestAccountCutoff(SavepointCase):
-
     def test_default_cutoff_account_id(self):
-        account_id = \
-            self.env['account.cutoff']._default_cutoff_account_id()
+        account_id = self.env["account.cutoff"]._default_cutoff_account_id()
 
-        self.assertIsNone(account_id, 'The account must be none')
+        self.assertIsNone(account_id, "The account must be none")
