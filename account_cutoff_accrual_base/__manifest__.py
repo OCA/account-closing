2057a99b459fe6f6f849fa523a5214277a512ec5
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-24 01:02:43 +0200

    MIG account_cutoff_accrual_* from v12 to v13

--- account_cutoff_accrual_base/__manifest__.py
+++ account_cutoff_accrual_base/__manifest__.py
@@ -1,15 +1,16 @@
-# Copyright 2013-2019 Akretion France
+# Copyright 2013-2020 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Accrual Base',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Base module for accrued expenses and revenues',
     'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
+    "maintainers": ["alexis-via"],
+    'website': 'https://github.com/OCA/account-closing',
     'depends': ['account_cutoff_base'],
     'data': [
         'views/res_config_settings.xml',
