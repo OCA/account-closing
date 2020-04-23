028cd7f466f76646733b2a2505f0763110b745aa
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-24 01:02:43 +0200

    MIG account_cutoff_accrual_* from v12 to v13

--- account_cutoff_accrual_base/models/company.py
+++ account_cutoff_accrual_base/models/company.py
@@ -1,6 +1,6 @@
-# Copyright 2013-2018 Akretion (http://www.akretion.com)
-# Copyright 2017 ACSONE SA/NV
-# Copyright 2018 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
+# Copyright 2013-2020 Akretion (http://www.akretion.com)
+# Copyright 2017-2020 ACSONE SA/NV
+# Copyright 2018-2020 Jacques-Etienne Baudoux (BCIM sprl) <je@bcim.be>
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -15,27 +15,9 @@ class ResCompany(models.Model):
         string='Default Account for Accrued Revenues',
         domain=[('deprecated', '=', False)])
 
-    default_accrued_revenue_return_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Default Account for Accrued Revenues Returns',
-        domain=[('deprecated', '=', False)])
-
     default_accrued_expense_account_id = fields.Many2one(
         comodel_name='account.account',
         string='Default Account for Accrued Expenses',
         domain=[('deprecated', '=', False)])
 
-    default_accrued_expense_return_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Default Account for Accrued Expenses Returns',
-        domain=[('deprecated', '=', False)])
-
-    default_accrual_revenue_journal_id = fields.Many2one(
-        comodel_name='account.journal',
-        string='Default Journal for Accrued Revenues')
-
-    default_accrual_expense_journal_id = fields.Many2one(
-        comodel_name='account.journal',
-        string='Default Journal for Accrued Expenses')
-
-    accrual_taxes = fields.Boolean(string='Accrual On Taxes')
+    accrual_taxes = fields.Boolean(string='Accrual On Taxes', default=True)
