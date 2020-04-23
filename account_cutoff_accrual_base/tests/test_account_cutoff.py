1110ec6b10769cf39dc01175628c24760769bb7e
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-24 01:08:51 +0200

    Black and other pre-commit stuff

--- account_cutoff_accrual_base/tests/test_account_cutoff.py
+++ account_cutoff_accrual_base/tests/test_account_cutoff.py
@@ -6,21 +6,32 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountCutoff(TransactionCase):
-
     def test_default_cutoff_account_id(self):
         company = self.env.user.company_id
-        default_accrued_expense_account_id = \
+        default_accrued_expense_account_id = (
             company.default_accrued_expense_account_id.id or False
-        default_accrued_revenue_account_id = \
+        )
+        default_accrued_revenue_account_id = (
             company.default_accrued_revenue_account_id.id or False
+        )
 
-        account_id = self.env['account.cutoff'].with_context(
-            cutoff_type='accrued_expense')._default_cutoff_account_id()
-        self.assertEqual(account_id, default_accrued_expense_account_id,
-                         'The account must be equals to %s' %
-                         default_accrued_expense_account_id)
-        account_id = self.env['account.cutoff'].with_context(
-            cutoff_type='accrued_revenue')._default_cutoff_account_id()
-        self.assertEqual(account_id, default_accrued_revenue_account_id,
-                         'The account must be equals to %s' %
-                         default_accrued_revenue_account_id)
+        account_id = (
+            self.env["account.cutoff"]
+            .with_context(cutoff_type="accrued_expense")
+            ._default_cutoff_account_id()
+        )
+        self.assertEqual(
+            account_id,
+            default_accrued_expense_account_id,
+            "The account must be equals to %s" % default_accrued_expense_account_id,
+        )
+        account_id = (
+            self.env["account.cutoff"]
+            .with_context(cutoff_type="accrued_revenue")
+            ._default_cutoff_account_id()
+        )
+        self.assertEqual(
+            account_id,
+            default_accrued_revenue_account_id,
+            "The account must be equals to %s" % default_accrued_revenue_account_id,
+        )
