7eb1a96ed8e3f290a7e9a1d485682f0fe21b44a1
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-24 01:08:51 +0200

    Black and other pre-commit stuff

--- account_cutoff_accrual_base/models/account_tax.py
+++ account_cutoff_accrual_base/models/account_tax.py
@@ -7,15 +7,15 @@ from odoo import fields, models
 
 
 class AccountTax(models.Model):
-    _inherit = 'account.tax'
+    _inherit = "account.tax"
 
     account_accrued_revenue_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Accrued Revenue Tax Account',
-        domain=[('deprecated', '=', False)]
+        comodel_name="account.account",
+        string="Accrued Revenue Tax Account",
+        domain=[("deprecated", "=", False)],
     )
     account_accrued_expense_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Accrued Expense Tax Account',
-        domain=[('deprecated', '=', False)]
+        comodel_name="account.account",
+        string="Accrued Expense Tax Account",
+        domain=[("deprecated", "=", False)],
     )
