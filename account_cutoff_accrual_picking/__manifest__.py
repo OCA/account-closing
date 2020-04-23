f9c778eecf69c8d436dd365b7a55f4d12d18e0a5
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-24 01:02:43 +0200

    MIG account_cutoff_accrual_* from v12 to v13

--- account_cutoff_accrual_picking/__manifest__.py
+++ account_cutoff_accrual_picking/__manifest__.py
@@ -1,16 +1,17 @@
-# Copyright 2013-2019 Akretion France (http://www.akretion.com)
+# Copyright 2013-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Cut-off Accrual Picking',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Accrued expense & accrued revenue from pickings',
     'author': "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
     'website': 'https://github.com/OCA/account-closing',
-    'depends': ['account_cutoff_accrual_base', 'purchase', 'sale_stock'],
+    'depends': ['account_cutoff_accrual_base', 'purchase_stock', 'sale_stock'],
     'images': [
         'images/accrued_expense_draft.jpg',
         'images/accrued_expense_journal_entry.jpg',
