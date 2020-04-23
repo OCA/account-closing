5e923e7fd8cfc94b84583756509969287b6e5216
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-24 01:08:51 +0200

    Black and other pre-commit stuff

--- account_cutoff_accrual_picking/__manifest__.py
+++ account_cutoff_accrual_picking/__manifest__.py
@@ -3,20 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Cut-off Accrual Picking',
-    'version': '13.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'summary': 'Accrued expense & accrued revenue from pickings',
-    'author': "Akretion,Odoo Community Association (OCA)",
+    "name": "Account Cut-off Accrual Picking",
+    "version": "13.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Accrued expense & accrued revenue from pickings",
+    "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
-    'website': 'https://github.com/OCA/account-closing',
-    'depends': ['account_cutoff_accrual_base', 'purchase_stock', 'sale_stock'],
-    'images': [
-        'images/accrued_expense_draft.jpg',
-        'images/accrued_expense_journal_entry.jpg',
-        'images/accrued_expense_done.jpg',
-        ],
-    'installable': True,
-    'application': True,
+    "website": "https://github.com/OCA/account-closing",
+    "depends": ["account_cutoff_accrual_base", "purchase_stock", "sale_stock"],
+    "images": [
+        "images/accrued_expense_draft.jpg",
+        "images/accrued_expense_journal_entry.jpg",
+        "images/accrued_expense_done.jpg",
+    ],
+    "installable": True,
+    "application": True,
 }
