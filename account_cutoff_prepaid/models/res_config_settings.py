72854d8e0206891c0db97e515222ad94d96d2e6e
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-23 20:21:20 +0200

    Black and other pre-commit stuff

--- account_cutoff_prepaid/models/res_config_settings.py
+++ account_cutoff_prepaid/models/res_config_settings.py
@@ -6,11 +6,11 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     dft_prepaid_revenue_account_id = fields.Many2one(
-        related='company_id.default_prepaid_revenue_account_id',
-        readonly=False)
+        related="company_id.default_prepaid_revenue_account_id", readonly=False
+    )
     dft_prepaid_expense_account_id = fields.Many2one(
-        related='company_id.default_prepaid_expense_account_id',
-        readonly=False)
+        related="company_id.default_prepaid_expense_account_id", readonly=False
+    )
