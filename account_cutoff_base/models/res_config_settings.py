06f6084aa43a0ee9704e91e6311d7e64f382cae6
Alexis de Lattre <alexis.delattre@akretion.com>
2020-04-23 20:21:20 +0200

    Black and other pre-commit stuff

--- account_cutoff_base/models/res_config_settings.py
+++ account_cutoff_base/models/res_config_settings.py
@@ -6,9 +6,10 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     # I can't name it default_cutoff_journal_id
     # because default_ is a special prefix
     dft_cutoff_journal_id = fields.Many2one(
-        related='company_id.default_cutoff_journal_id', readonly=False)
+        related="company_id.default_cutoff_journal_id", readonly=False
+    )
