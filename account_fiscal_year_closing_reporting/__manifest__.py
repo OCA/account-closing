fd7d19b7fc5cbf8eb65a5e252e013068842a0a3f
sergiocorato <sergiocorato@gmail.com>
2020-04-25 15:58:13 +0200

    [IMP] add glue module to fix general ledger reporting with closing moves

---
+++ account_fiscal_year_closing_reporting/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fiscal year closing reporting",
+    "summary": "Remove fyc moves from account reporting",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://efatto.it/",
+    "author": "Sergio Corato, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_fiscal_year_closing",
+        "account_financial_report",
+    ],
+    "data": [
+    ],
+    "auto_install": True,
+}
---
+++ account_fiscal_year_closing_reporting/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This is a glue module to mimic the old behaviour of
+printing account report without closing moves, as
+period exists no more.
