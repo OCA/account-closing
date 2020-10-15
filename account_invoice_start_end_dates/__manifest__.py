316e933e169731d8190ed553ffeb828629699839
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-15 23:29:07 +0200

    [MIG] account_invoice_start_end_dates from 13 to 14

--- account_invoice_start_end_dates/__manifest__.py
+++ account_invoice_start_end_dates/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2016-2019 Akretion France
-# Copyright 2018-2019 Camptocamp
+# Copyright 2016-2020 Akretion France (http://www.akretion.com/)
+# Copyright 2018-2020 Camptocamp
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Invoice Start End Dates",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Adds start/end dates on invoice/move lines",
@@ -13,7 +13,7 @@
     "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/account-closing",
     "depends": ["account"],
-    "data": ["views/account_move.xml", "views/product.xml"],
+    "data": ["views/account_move.xml", "views/product_template.xml"],
     "demo": ["demo/product_demo.xml"],
     "installable": True,
 }
