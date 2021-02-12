53a91e427067fdafa9d1c125fc88992f4fd1f523
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-12 16:00:05 +0100

    [MIG] account_cutoff_base to v14
    Now include account_cutoff_accrual_base, as suggested in issue #133

--- account_cutoff_base/models/__init__.py
+++ account_cutoff_base/models/__init__.py
@@ -1,3 +1,4 @@
-from . import company
+from . import res_company
+from . import account_tax
 from . import account_cutoff
-from . import res_config_settings
+from . import account_cutoff_mapping
---
+++ account_cutoff_base/models/account_tax.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2021 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class AccountTax(models.Model):
+    _inherit = "account.tax"
+
+    account_accrued_revenue_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Accrued Revenue Tax Account",
+        domain=[("deprecated", "=", False)],
+        check_company=True,
+    )
+    account_accrued_expense_id = fields.Many2one(
+        comodel_name="account.account",
+        string="Accrued Expense Tax Account",
+        domain=[("deprecated", "=", False)],
+        check_company=True,
+    )
