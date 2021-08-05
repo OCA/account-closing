f87166e8d55b2879f080767ae9071858aef11fed
Denis Roussel <denis.roussel@acsone.eu>
2021-08-05 16:35:01 +0200

    [14.0][FIX] account_cutoff_base: Move domains where they are used
    Avoid ValidationError on res.company views update as company_id field
    does not exist there

--- account_cutoff_base/wizards/res_config_settings.py
+++ account_cutoff_base/wizards/res_config_settings.py
@@ -11,21 +11,31 @@ class ResConfigSettings(models.TransientModel):
     # I can't name it default_cutoff_journal_id
     # because default_ is a special prefix
     dft_cutoff_journal_id = fields.Many2one(
-        related="company_id.default_cutoff_journal_id", readonly=False
+        related="company_id.default_cutoff_journal_id",
+        readonly=False,
+        domain="[('type', '=', 'general'), ('company_id', '=', company_id)]",
     )
     dft_cutoff_move_partner = fields.Boolean(
         related="company_id.default_cutoff_move_partner", readonly=False
     )
     accrual_taxes = fields.Boolean(related="company_id.accrual_taxes", readonly=False)
     dft_accrued_revenue_account_id = fields.Many2one(
-        related="company_id.default_accrued_revenue_account_id", readonly=False
+        related="company_id.default_accrued_revenue_account_id",
+        readonly=False,
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
     )
     dft_accrued_expense_account_id = fields.Many2one(
-        related="company_id.default_accrued_expense_account_id", readonly=False
+        related="company_id.default_accrued_expense_account_id",
+        readonly=False,
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
     )
     dft_prepaid_revenue_account_id = fields.Many2one(
-        related="company_id.default_prepaid_revenue_account_id", readonly=False
+        related="company_id.default_prepaid_revenue_account_id",
+        readonly=False,
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
     )
     dft_prepaid_expense_account_id = fields.Many2one(
-        related="company_id.default_prepaid_expense_account_id", readonly=False
+        related="company_id.default_prepaid_expense_account_id",
+        readonly=False,
+        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
     )
