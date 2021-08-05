ddaed68f67ab47c6b4687cafb719e6c9dfd3b2ac
Denis Roussel <denis.roussel@acsone.eu>
2021-08-05 16:35:01 +0200

    [14.0][FIX] account_cutoff_base: Move domains where they are used
    Avoid ValidationError on res.company views update as company_id field
    does not exist there

--- account_cutoff_base/models/res_company.py
+++ account_cutoff_base/models/res_company.py
@@ -13,7 +13,6 @@ class ResCompany(models.Model):
     default_cutoff_journal_id = fields.Many2one(
         "account.journal",
         string="Default Cut-off Journal",
-        domain="[('type', '=', 'general'), ('company_id', '=', company_id)]",
         check_company=True,
     )
     default_cutoff_move_partner = fields.Boolean(
@@ -23,24 +22,20 @@ class ResCompany(models.Model):
     default_accrued_revenue_account_id = fields.Many2one(
         comodel_name="account.account",
         string="Default Account for Accrued Revenues",
-        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
         check_company=True,
     )
     default_accrued_expense_account_id = fields.Many2one(
         comodel_name="account.account",
         string="Default Account for Accrued Expenses",
-        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
         check_company=True,
     )
     default_prepaid_revenue_account_id = fields.Many2one(
         "account.account",
         string="Default Account for Prepaid Revenue",
-        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
         check_company=True,
     )
     default_prepaid_expense_account_id = fields.Many2one(
         "account.account",
         string="Default Account for Prepaid Expense",
-        domain="[('deprecated', '=', False), ('company_id', '=', company_id)]",
         check_company=True,
     )
