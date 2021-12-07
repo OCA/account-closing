1e453314ea46086cfbfe88ee9126a304cec84c74
Alexis de Lattre <alexis.delattre@akretion.com>
2021-12-07 09:30:23 +0100

    [MIG] account_invoice_start_end_dates to v15

--- account_invoice_start_end_dates/models/account_move.py
+++ account_invoice_start_end_dates/models/account_move.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Akretion France (https://akretion.com/)
+# Copyright 2019-2021 Akretion France (https://akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
