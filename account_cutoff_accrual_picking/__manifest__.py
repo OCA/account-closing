2be5bb755bd7317cede78f85c518ba6be6b5bdd9
Maksym Yankin <yankinmk@gmail.com>
2022-01-24 15:32:03 +0200

    [MIG] account_cutoff_accrual_picking: Migration to 15.0

--- account_cutoff_accrual_picking/__manifest__.py
+++ account_cutoff_accrual_picking/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Cut-off Accrual Picking",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Accrued expense & accrued revenue from pickings",
