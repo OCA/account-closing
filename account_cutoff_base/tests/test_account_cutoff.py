7188c92f22709c6606bb917d1c1dabd5f43e5e32
Maksym Yankin <yankinmk@gmail.com>
2022-01-24 14:22:14 +0200

    [MIG] account_cutoff_base: Migration to 15.0

--- account_cutoff_base/tests/test_account_cutoff.py
+++ account_cutoff_base/tests/test_account_cutoff.py
@@ -1,9 +1,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestAccountCutoff(SavepointCase):
+class TestAccountCutoff(TransactionCase):
     def test_default_cutoff_account_id(self):
         account_id = self.env["account.cutoff"]._default_cutoff_account_id()
         self.assertEqual(account_id, False)
