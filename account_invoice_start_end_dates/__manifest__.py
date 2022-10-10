2e53b7e112466ef2913f597c64e3483396d4ba77
Maxime Franco <maxime.franco@acsone.eu>
2022-10-10 12:21:24 +0200

    [MIG] account_invoice_start_end_dates: Migration to 16.0

--- account_invoice_start_end_dates/__manifest__.py
+++ account_invoice_start_end_dates/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Account Invoice Start End Dates",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting & Finance",
     "license": "AGPL-3",
     "summary": "Adds start/end dates on invoice/move lines",
