f986b9566ad13a74b4e021072d3b0bf27d3ff096
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-14 14:02:43 +0200

    subscription: cut long method in 2
    Use format_date() and format_amount() in cut logs

--- account_cutoff_accrual_subscription/models/account_cutoff_accrual_subscription.py
+++ account_cutoff_accrual_subscription/models/account_cutoff_accrual_subscription.py
@@ -2,8 +2,10 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from dateutil.relativedelta import relativedelta
+
 from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 
 
 class AccountCutoffAccrualSubscription(models.Model):
@@ -128,3 +130,121 @@ class AccountCutoffAccrualSubscription(models.Model):
     def partner_type_change(self):
         if self.partner_type != "one":
             self.partner_id = False
+
+    def _process_subscription(self, work, end_date, common_domain, sign):
+        self.ensure_one()
+        aml_obj = self.env["account.move.line"]
+        periodicity2months = {
+            "month": 1,
+            "quarter": 3,
+            "semester": 6,
+            "year": 12,
+        }
+        company = self.company_id
+        ccur = company.currency_id
+        months = periodicity2months[self.periodicity]
+        work[self] = {"intervals": [], "sub": self}
+        domain_base = common_domain + [
+            ("company_id", "=", company.id),
+            ("account_id", "=", self.account_id.id),
+            ("analytic_account_id", "=", self.analytic_account_id.id or False),
+        ]
+        if self.partner_type == "one":
+            if self.partner_id:
+                domain_base.append(("partner_id", "=", self.partner_id.id))
+            else:
+                raise UserError(
+                    _("Missing supplier on subscription '%s'.") % self.display_name
+                )
+        elif self.partner_type == "none":
+            domain_base.append(("partner_id", "=", False))
+        domain_base_w_start_end = domain_base + [
+            ("start_date", "!=", False),
+            ("end_date", "!=", False),
+        ]
+
+        for _i in range(int(12 / months)):
+            start_date = end_date + relativedelta(day=1, months=-(months - 1))
+            if start_date < self.start_date:
+                break
+            # compute amount
+            amount = 0
+            # 1. No start/end dates
+            no_start_end_res = aml_obj.read_group(
+                domain_base
+                + [
+                    ("date", "<=", end_date),
+                    ("date", ">=", start_date),
+                    ("start_date", "=", False),
+                    ("end_date", "=", False),
+                ],
+                ["balance"],
+                [],
+            )
+            amount_no_start_end = (
+                no_start_end_res and no_start_end_res[0]["balance"] or 0
+            )
+            amount += amount_no_start_end * sign
+            # 2. Start/end dates, INSIDE interval
+            inside_res = aml_obj.read_group(
+                domain_base_w_start_end
+                + [
+                    ("start_date", ">=", start_date),
+                    ("end_date", "<=", end_date),
+                ],
+                ["balance"],
+                [],
+            )
+            amount_inside = inside_res and inside_res[0]["balance"] or 0
+            amount += amount_inside * sign
+            # 3. Start/end dates, OVER interval
+            mlines = aml_obj.search(
+                domain_base_w_start_end
+                + [
+                    ("start_date", "<", start_date),
+                    ("end_date", ">", end_date),
+                ]
+            )
+            for mline in mlines:
+                total_days = (mline.end_date - mline.start_date).days + 1
+                days_in_interval = (end_date - start_date).days + 1
+                amount_in_interval = mline.balance * days_in_interval / total_days
+                amount += amount_in_interval * sign
+            # 4. Start/end dates, start_date before, end_date inside
+            mlines = aml_obj.search(
+                domain_base_w_start_end
+                + [
+                    ("start_date", "<", start_date),
+                    ("end_date", ">=", start_date),
+                    ("end_date", "<=", end_date),
+                ]
+            )
+            for mline in mlines:
+                total_days = (mline.end_date - mline.start_date).days + 1
+                days_in_interval = (mline.end_date - start_date).days + 1
+                amount_in_interval = mline.balance * days_in_interval / total_days
+                amount += amount_in_interval * sign
+            # 5. Start/end dates, start_date inside, end_date after
+            mlines = aml_obj.search(
+                domain_base_w_start_end
+                + [
+                    ("start_date", ">=", start_date),
+                    ("start_date", "<=", end_date),
+                    ("end_date", ">", end_date),
+                ]
+            )
+            for mline in mlines:
+                total_days = (mline.end_date - mline.start_date).days + 1
+                days_in_interval = (end_date - mline.start_date).days + 1
+                amount_in_interval = mline.balance * days_in_interval / total_days
+                amount += amount_in_interval * sign
+
+            work[self]["intervals"].append(
+                {
+                    "start": start_date,
+                    "end": end_date,
+                    "amount": ccur.round(amount),
+                }
+            )
+            # prepare next interval
+            end_date = start_date + relativedelta(days=-1)
