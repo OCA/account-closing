6e14b4344e322b8d7120cf7e20c79f40a8770062
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2022-10-25 14:16:00 +0200

    [IMP]account_multicurrency_revaluation: Improve company and related fields

--- account_multicurrency_revaluation/model/res_config.py
+++ account_multicurrency_revaluation/model/res_config.py
@@ -9,101 +9,54 @@ class AccountConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     revaluation_loss_account_id = fields.Many2one(
-        related="company_id.revaluation_loss_account_id",
         comodel_name="account.account",
-        string="Revaluation loss account",
-        domain=lambda self: [
-            ("company_id", "in", self.env.company.id),
-            ("internal_type", "=", "other"),
-            ("deprecated", "=", False),
-        ],
+        related="company_id.revaluation_loss_account_id",
         readonly=False,
     )
     revaluation_gain_account_id = fields.Many2one(
-        related="company_id.revaluation_gain_account_id",
         comodel_name="account.account",
-        string="Revaluation gain account",
-        domain=lambda self: [
-            ("company_id", "in", self.env.company.id),
-            ("internal_type", "=", "other"),
-            ("deprecated", "=", False),
-        ],
+        related="company_id.revaluation_gain_account_id",
         readonly=False,
     )
     revaluation_analytic_account_id = fields.Many2one(
-        related="company_id.revaluation_analytic_account_id",
         comodel_name="account.analytic.account",
-        string="Revaluation Analytic account",
+        related="company_id.revaluation_analytic_account_id",
         readonly=False,
     )
     provision_bs_loss_account_id = fields.Many2one(
-        related="company_id.provision_bs_loss_account_id",
         comodel_name="account.account",
-        string="Provision B.S. loss account",
-        domain=lambda self: [
-            ("company_id", "in", self.env.company.id),
-            ("internal_type", "=", "other"),
-            ("deprecated", "=", False),
-        ],
+        related="company_id.provision_bs_loss_account_id",
         readonly=False,
     )
     provision_bs_gain_account_id = fields.Many2one(
-        related="company_id.provision_bs_gain_account_id",
         comodel_name="account.account",
-        string="Provision B.S. gain account",
-        domain=lambda self: [
-            ("company_id", "in", self.env.company.id),
-            ("internal_type", "=", "other"),
-            ("deprecated", "=", False),
-        ],
+        related="company_id.provision_bs_gain_account_id",
         readonly=False,
     )
     provision_pl_loss_account_id = fields.Many2one(
-        related="company_id.provision_pl_loss_account_id",
         comodel_name="account.account",
-        string="Provision P&L loss account",
-        domain=lambda self: [
-            ("company_id", "in", self.env.company.id),
-            ("internal_type", "=", "other"),
-            ("deprecated", "=", False),
-        ],
+        related="company_id.provision_pl_loss_account_id",
         readonly=False,
     )
     provision_pl_gain_account_id = fields.Many2one(
-        related="company_id.provision_pl_gain_account_id",
         comodel_name="account.account",
-        string="Provision P&L gain account",
-        domain=lambda self: [
-            ("company_id", "in", self.env.company.id),
-            ("internal_type", "=", "other"),
-            ("deprecated", "=", False),
-        ],
+        related="company_id.provision_pl_gain_account_id",
         readonly=False,
     )
     provision_pl_analytic_account_id = fields.Many2one(
-        related="company_id.provision_pl_analytic_account_id",
         comodel_name="account.analytic.account",
-        string="Provision P&L Analytic account",
+        related="company_id.provision_pl_analytic_account_id",
         readonly=False,
     )
     default_currency_reval_journal_id = fields.Many2one(
-        related="company_id.currency_reval_journal_id",
         comodel_name="account.journal",
-        string="Currency gain & loss Default Journal",
-        domain=[("type", "=", "general")],
+        related="company_id.currency_reval_journal_id",
         default_model="res.company",
         readonly=False,
     )
     reversable_revaluations = fields.Boolean(
-        related="company_id.reversable_revaluations",
-        string="Reversable Revaluations",
-        help="Revaluations entries will be created " 'as "To Be Reversed".',
-        readonly=False,
+        related="company_id.reversable_revaluations", readonly=False,
     )
     auto_post_entries = fields.Boolean(
-        related="company_id.auto_post_entries",
-        string="Auto Post Created Entries",
-        help="If marked, the entries created in the process will be"
-        "posted automatically.",
-        readonly=False,
+        related="company_id.auto_post_entries", readonly=False,
     )
