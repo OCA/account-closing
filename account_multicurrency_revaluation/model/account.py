1e59a112a130dc6e5bb157c288b61bff3f992f04
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2022-12-02 17:07:09 +0100

    [IMP]account_multicurrency_revaluation: Adjust values if partial reconciled items If the origin line that will be revaluated has partially reconciled items, adjust the values to the real ones.

--- account_multicurrency_revaluation/model/account.py
+++ account_multicurrency_revaluation/model/account.py
@@ -244,14 +244,37 @@ ORDER BY account_id, partner_id, currency_id"""
 
         data = {}
         for line in lines:
-            account_id, currency_id, partner_id = (
+            account_id, currency_id, partner_id, origin_aml_id = (
                 line["id"],
                 line["currency_id"],
                 line["partner_id"],
+                line["origin_aml_id"],
             )
             data.setdefault(account_id, {})
             data[account_id].setdefault(partner_id, {})
             data[account_id][partner_id].setdefault(currency_id, {})
+            # If partially reconciled, we need to adjust the balance according to
+            # the partially reconciled items on the current line.
+            origin_aml = self.env["account.move.line"].browse(origin_aml_id)
+            if origin_aml.matched_debit_ids | origin_aml.matched_credit_ids:
+                debit_move_ids = origin_aml.matched_debit_ids.mapped("debit_move_id")
+                credit_move_ids = origin_aml.matched_credit_ids.mapped("credit_move_id")
+                total_debit = line["debit"] + sum(debit_move_ids.mapped("debit"))
+                total_credit = line["credit"] + sum(credit_move_ids.mapped("credit"))
+                total_balance = total_debit - total_credit
+                total_balance_currency = (
+                    line["foreign_balance"]
+                    + sum(debit_move_ids.mapped("amount_currency"))
+                    + sum(credit_move_ids.mapped("amount_currency"))
+                )
+                line.update(
+                    {
+                        "debit": round(total_debit, 2),
+                        "credit": round(total_credit, 2),
+                        "balance": round(total_balance, 2),
+                        "foreign_balance": round(total_balance_currency, 2),
+                    }
+                )
             existing_line = data[account_id][partner_id][currency_id]
             if existing_line:
                 data[account_id][partner_id][
