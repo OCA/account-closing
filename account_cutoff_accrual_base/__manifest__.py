ed5230c921ba93cd0287d9547b60e27167dbd936
Luca D'Ambrosio <91986538+Dambrous@users.noreply.github.com>
2023-01-02 10:31:09 +0100

    [MIG] account_cutoff_accrual_base: Migration to 14.0

--- account_cutoff_accrual_base/__manifest__.py
+++ account_cutoff_accrual_base/__manifest__.py
@@ -4,18 +4,18 @@
 
 {
     "name": "Account Accrual Base",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Base module for accrued expenses and revenues",
-    "author": "Akretion,Odoo Community Association (OCA)",
+    "author": "Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/account-closing",
     "depends": ["account_cutoff_base"],
     "data": [
-        "views/res_config_settings.xml",
-        "views/account_tax.xml",
-        "views/account_cutoff_view.xml",
+        "views/res_config_settings_views.xml",
+        "views/account_tax_views.xml",
+        "views/account_cutoff_views.xml",
     ],
     "installable": True,
 }
--- account_cutoff_accrual_base/models/__init__.py
+++ account_cutoff_accrual_base/models/__init__.py
@@ -1,4 +1,4 @@
-from . import company
+from . import res_company
 from . import res_config_settings
 from . import account_tax
 from . import account_cutoff
