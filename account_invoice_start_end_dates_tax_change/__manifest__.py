e99ab0cf3661cb08cc022796f0dbbbd10f3aa6b8
Sébastien Alix <sebastien.alix@camptocamp.com>
2023-04-05 17:21:12 +0200

    a_i_start_end_dates_tax_change: glue with 'account_tax_change'

---
+++ account_invoice_start_end_dates_tax_change/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Account Invoice Start End Dates - Tax Changes",
+    "version": "16.0.1.0.0",
+    "summary": "Adds start/end dates on invoice/move lines - Tax changes integration",
+    "category": "Accounting & Finance",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-closing",
+    "depends": ["account_invoice_start_end_dates", "account_tax_change"],
+    "installable": True,
+    "auto_install": True,
+    "development_status": "Beta",
+    "maintainers": ["sebalix"],
+}
---
+++ account_invoice_start_end_dates_tax_change/wizards/account_move_apply_tax_change.py
@@ -0,0 +1,82 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class AccountMoveApplyTaxChange(models.TransientModel):
+    _inherit = "account.move.apply.tax.change"
+
+    def _skip_line(self, line):
+        if self._is_recurring_line(line):
+            if self._is_recurring_tax_change_required(line):
+                return False
+            return True
+        return super()._skip_line(line)
+
+    def _is_recurring_line(self, line):
+        return line.start_date and line.end_date
+
+    def _is_recurring_tax_change_required(self, line):
+        # Start & end dates defined in the scope of the tax changes
+        return line.start_date <= self.tax_change_id.date < line.end_date
+
+    def _change_taxes(self, line):
+        if self._is_recurring_line(line):
+            if self._is_recurring_tax_change_required(line):
+                # Split the invoice line by applying a prorata on the unit price
+                # NOTE: if the tax change occurs on 2024-02-01, the end date
+                # of the split invoice line will be the day before (2024-01-31)
+                # such as it doesn't overlap with the start date of the new line.
+                new_end_date = fields.Date.subtract(self.tax_change_id.date, days=1)
+                # Dates subtractions are not inclusives so we add +1 day each time
+                total_days = (line.end_date - line.start_date).days + 1
+                days_until_change = (new_end_date - line.start_date).days + 1
+                price_unit1 = line.price_unit * days_until_change / total_days
+                price_unit2 = line.price_unit - price_unit1
+                new_line_vals = self._prepare_new_line_values(line, price_unit2)
+                line_vals = self._prepare_existing_line_values(line, price_unit1)
+                new_line = line.create(new_line_vals)
+                new_line = self._hook_new_line_before_change_taxes(new_line)
+                # Update the taxes only on the new line
+                res = super()._change_taxes(new_line)
+                new_line = self._hook_new_line_after_change_taxes(new_line)
+                # Update the existing line that keep the current tax
+                line.write(line_vals)
+                lines = line | new_line
+                lines.invalidate_recordset()
+                lines.modified(["start_date", "end_date", "price_unit"])
+                lines.flush_recordset()
+                return res
+            # Start & end dates defined but not in the scope of the tax changes
+            return
+        return super()._change_taxes(line)
+
+    def _prepare_new_line_values(self, existing_line, price_unit):
+        return existing_line.copy_data(
+            {
+                "start_date": self.tax_change_id.date,
+                "end_date": existing_line.end_date,
+                "price_unit": price_unit,
+            }
+        )
+
+    def _prepare_existing_line_values(self, line, price_unit):
+        return {
+            "end_date": self.tax_change_id.date,
+            "price_unit": price_unit,
+        }
+
+    def _hook_new_line_before_change_taxes(self, line):
+        """Hook to override. Return the line record.
+
+        Called once the new line is created but the new tax is still not applied.
+        """
+        return line
+
+    def _hook_new_line_after_change_taxes(self, line):
+        """Hook to override. Return the line record.
+
+        Called once the new tax is applied on the new created line.
+        """
+        return line
