b40237d755b2f275c29d17e11b96ebad21c35e5f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-03-14 20:41:08 +0100

    [REF] account_fiscal_year_closing: Debrand module
    Although we originally developped the module, we figure out soon
    that this approach is not correct, so we prefer to not appearing
    in the credits.

--- account_fiscal_year_closing/__manifest__.py
+++ account_fiscal_year_closing/__manifest__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Tecnativa - Antonio Espinosa
-# Copyright 2016-2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -9,8 +7,7 @@
     "version": "10.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://www.tecnativa.org/",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
