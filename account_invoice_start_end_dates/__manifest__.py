7e895c4d2b58d58c23d27d168a962bae9c32b7aa
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2024-07-09 18:50:49 +0200

    [LIC] account_invoice_start_end_dates: change to LGPL

--- account_invoice_start_end_dates/__manifest__.py
+++ account_invoice_start_end_dates/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2016-2021 Akretion France (http://www.akretion.com/)
 # Copyright 2018-2021 Camptocamp
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Account Invoice Start End Dates",
     "version": "16.0.1.2.0",
     "category": "Accounting & Finance",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "summary": "Adds start/end dates on invoice/move lines",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainers": ["alexis-via"],
